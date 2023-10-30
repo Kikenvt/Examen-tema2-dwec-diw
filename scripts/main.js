5a9d114bace0d7769d35d9794ad790d7eda84483
Enrique Fdez-Campoamor <enriquefernandez-campoamorfernandez@alumnos.tuniversformacion.es>
2023-10-30 09:15:40 +0100

    Creada funcion de retirar dinero

--- scripts/main.js
+++ scripts/main.js
@@ -12,7 +12,7 @@ let intentosRestantes = 3
 // Enlazamos las las partes del template con JS
 
 const deposit = document.getElementById("deposit")
-const withraow = document.getElementById("withraow")
+const withraw = document.getElementById("withraw")
 const transfer = document.getElementById("transfer")
 const changePswd = document.getElementById("change-pswd")
 const exit = document.getElementById("exit")
@@ -20,7 +20,7 @@ const resultTemplate = document.getElementById("result")
 
 // Aqui añadiremos los listener para los eventos.
 deposit.addEventListener("click", depositMoney)
-
+withraw.addEventListener("click", withrawMoney)
 
 
 // Creamos las funciones:
@@ -36,12 +36,25 @@ function refreshAccount(){
 // 1-Depositar
 
 function depositMoney(){
-    const deposito = parseFloat(prompt('Por favor, indique la cantidad que desea retirar: '))
-    if(isNaN(deposito) || deposito <= 0){
+    const depositAmount = parseFloat(prompt('Por favor, indique la cantidad que desea depositar: '))
+    if(isNaN(depositAmount) || depositAmount <= 0){
         alert('Importe no válido')
     }else{
-        saldo -= deposito
-        alert(`Se ha depositado una cantidad de ${deposito} €`)
+        saldo += depositAmount
+        alert(`Se ha depositado una cantidad de ${depositAmount} €`)
+        refreshAccount()
+    }
+}
+
+// 2- Retirar
+
+function withrawMoney(){
+    const withrawAmount = parseFloat(prompt(`Por favor, indique la cantidad que desea retirar: `))
+    if(isNaN(withrawAmount) || withrawAmount <= 0 || withrawAmount > saldo){
+        alert('Saldo insuficiente o cántidad no válida')
+    }else{
+        saldo -= withrawAmount
+        alert(`Se ha retirado al cantidad de ${withrawAmount} €`)
         refreshAccount()
     }
 }
