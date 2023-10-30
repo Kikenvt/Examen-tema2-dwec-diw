464ebebb1b8495085f0dbc41d7a8369540e73dbc
Enrique Fdez-Campoamor <enriquefernandez-campoamorfernandez@alumnos.tuniversformacion.es>
2023-10-30 10:11:20 +0100

    Añadida funcion cambio de PIN

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,14 @@
 **GitHub**: 
 */
 
+window.addEventListener("load", sigIn)
+
 // Creamos las variable de scope global
 
+
+
 let saldo = 1000
-const PIN_CORRECTO = "1234"
+let PIN_CORRECTO = "1234"
 let intentosRestantes = 3
 
 // Enlazamos las las partes del template con JS
@@ -14,7 +18,7 @@ let intentosRestantes = 3
 const deposit = document.getElementById("deposit")
 const withraw = document.getElementById("withraw")
 const transfer = document.getElementById("transfer")
-const changePswd = document.getElementById("change-pswd")
+const changePin = document.getElementById("change-pswd")
 const exit = document.getElementById("exit")
 const resultTemplate = document.getElementById("result")
 
@@ -22,6 +26,7 @@ const resultTemplate = document.getElementById("result")
 deposit.addEventListener("click", depositMoney)
 withraw.addEventListener("click", withrawMoney)
 transfer.addEventListener("click", transferMoney)
+changePin.addEventListener("click", changePswd)
 
 // Creamos las funciones:
 
@@ -63,14 +68,67 @@ function withrawMoney(){
 
 function transferMoney(){
     const transferAmount = parseFloat(prompt(`Ingrese la cantidad a transferir`))
-    const iban = prompt(`Ingrese el IBAN de destino`)
-    if(isNaN(transferAmount) || transferAmount >saldo ){
-
+    //const iban = prompt(`Ingrese el IBAN de destino`)
+    if(isNaN(transferAmount) || transferAmount >saldo){
         alert(`Importe no valido`)
-
+        return
+    }else if(!validariban(iban)){
+        alert("IBAN no valido")
+        return
     }else{
+
+    
         saldo -= transferAmount
         alert(`Se han transferido ${transferAmount} € a la cuenta ${iban}`)
         refreshAccount()
     }
+}
+
+// Funcion para iniciar sesion
+
+function sigIn(){
+    let pin = prompt("Ingrese su pin: ")
+
+    while(pin != PIN_CORRECTO && intentosRestantes > 1){
+        intentosRestantes--
+        alert(`PIN incorecto. Intetos restantes ${intentosRestantes}`)
+        pin = prompt("Ingrese su pin")
+        if(pin === PIN_CORRECTO){
+            alert("Bienvenido")
+        }else{
+            window.location.replace("/templates/cajeroBloqueado.html")
+        }
+
+    }
+
+}
+
+
+// Cambiar PIN
+
+function changePswd(){
+     const pinActual = prompt("Ingrese su pin actual")
+     if(pinActual === PIN_CORRECTO){
+        PIN_CORRECTO = prompt("Ingrese su nuevo PIN")
+        alert(`Su nuevo pin es ${PIN_CORRECTO}`)
+     }else{
+        alert("PIN INCORRECTO")
+     }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// Expresion regular para el IBAN
+function validateIBAN(iban){
+    let expresionRegular = /^(ES\d{22})$/;
+    return validateIBAN.test(iban)
 }
